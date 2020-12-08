247fb8e6f49b738daf9ea95e41172aee3180ce7c
margaritasar <margarita10sar@gmail.com>
2020-12-08 03:46:40 +0000

    Running code for a simple example

--- simple_navigation_goals/src/sequence_of_navigation_goals.py
+++ simple_navigation_goals/src/sequence_of_navigation_goals.py
@@ -15,9 +15,17 @@ class MoveBaseSeq():
     def __init__(self):
 
         rospy.init_node('move_base_sequence')
-        p_seq = [20,18,0,30,17,0,15,15,0]
+        p_seq = [8,19.6,0,15,19.6,0,21,19.6,0,8,8.2,0,15,8.2,0,22,8.2,0]
+	# refilling point's position
+	refill_p = [32.2,14,0]
+	self.refill_yaw = Quaternion(*(quaternion_from_euler(0, 0, 90*math.pi/180, axes='sxyz')))
+	self.refill_p = Pose(Point(*refill_p),self.refill_yaw) 
+	# amount of spray
+	self.spray_counter = 100
+	# flag to check if it's been to the refilling point
+	self.flag = 0
         # Only yaw angle required (no rotations around x and y axes) in deg:
-        yea_seq = [90,0,180]
+        yea_seq = [90,90,90,-90,-90,-90]
         #List of goal quaternions:
         quat_seq = list()
         #List of goal poses:
@@ -56,14 +64,30 @@ class MoveBaseSeq():
         rospy.loginfo("Feedback for goal pose "+str(self.goal_cnt+1)+" received")
 
     def done_callback(self, status, result):
-        self.goal_cnt += 1
     # Reference for terminal status values: http://docs.ros.org/diamondback/api/actionlib_msgs/html/msg/GoalStatus.html
         if status == 2:
+	    self.goal_cnt += 1
             rospy.loginfo("Goal pose "+str(self.goal_cnt)+" received a cancel request after it started executing, completed execution!")
 
         if status == 3:
-            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" reached") 
-            if self.goal_cnt< len(self.pose_seq):
+	    if self.flag:
+		rospy.loginfo("Ready for business again!")
+		self.flag = 0
+	    else:
+	    	self.goal_cnt += 1
+            	rospy.loginfo("Goal pose "+str(self.goal_cnt)+" reached")
+	    	rospy.loginfo("Spraying area!")
+	    	self.spray_counter -= 50
+	    if self.spray_counter == 0:
+	        rospy.loginfo("Spray can is empty! Heading to refilling station.")
+		self.flag = 1
+		self.spray_counter = 100
+		goal = MoveBaseGoal()
+		goal.target_pose.header.frame_id = "map"
+                goal.target_pose.header.stamp = rospy.Time.now()
+                goal.target_pose.pose = self.refill_p
+		self.client.send_goal(goal, self.done_callback)
+            elif self.goal_cnt < len(self.pose_seq):
                 next_goal = MoveBaseGoal()
                 next_goal.target_pose.header.frame_id = "map"
                 next_goal.target_pose.header.stamp = rospy.Time.now()
@@ -77,16 +101,19 @@ class MoveBaseSeq():
                 return
 
         if status == 4:
+	    self.goal_cnt += 1
             rospy.loginfo("Goal pose "+str(self.goal_cnt)+" was aborted by the Action Server")
             rospy.signal_shutdown("Goal pose "+str(self.goal_cnt)+" aborted, shutting down!")
             return
 
         if status == 5:
+	    self.goal_cnt += 1
             rospy.loginfo("Goal pose "+str(self.goal_cnt)+" has been rejected by the Action Server")
             rospy.signal_shutdown("Goal pose "+str(self.goal_cnt)+" rejected, shutting down!")
             return
 
         if status == 8:
+	    self.goal_cnt += 1
             rospy.loginfo("Goal pose "+str(self.goal_cnt)+" received a cancel request before it started executing, successfully cancelled!")
 
     def movebase_client(self):
